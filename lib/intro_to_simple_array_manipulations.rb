574003aa00c82f490a2aa604347ccad5af12f60d
Mark G <markghaida@gmail.com>
2020-11-08 00:34:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,63 +1,55 @@
-def using_push(countries_in_western_africa, next_country)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  next_country = "Niger"
-  countries_in_western_africa.push(next_country)
+def using_push(array, element)
+  array.push(element)
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  @new_neighborhood = "Brooklyn Heights"
-  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+def using_unshift(array, element)
+  array.unshift(element)
 end
 
-def using_pop(great_hits_of_the_nineties)
-  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  @great_hits_of_the_nineties.pop
+def using_pop(array)
+  array.pop
 end
 
-def pop_with_args(chars_in_game_of_thrones)
-  @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
-  @chars_in_game_of_thrones.pop(2)
+def pop_with_args(array)
+  result = []
+  2.times do
+    result.unshift(array.pop)
+  end
+  result
 end
 
-def using_shift(my_favorite_cities)
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  @im_so_over_this_city = @my_favorite_cities.shift
+def using_shift(array)
+  array.shift
 end
 
-def shift_with_args(ice_cream_brands)
-  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @brands_removed = @ice_cream_brands.shift(2)
+def shift_with_args(array)
+  result = []
+  2.times do
+    result << array.shift
+  end
+  result
 end
 
-def using_concat(my_favorite_things, more_favs)
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
-  @all_my_favs = @my_favorite_things.concat(more_favs)
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-  @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  @another_esoteric_language = "Malbolge"
-  @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
+def using_insert(array, element)
+  array.insert(4, element)
 end
 
-def using_uniq(captain_planet_and_the_planeteers)
-  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-  new_array = captain_planet_and_the_planeteers.uniq
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten(private_colleges_in_newyork)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-  flat_array = private_colleges_in_newyork.flatten
+def using_flatten(array)
+  array.flatten
 end
 
-def using_delete(my_list, my_element)
-  my_list.delete(my_element)
+def using_delete(array, element)
+  array.delete(element)
 end
 
-def using_delete_at(famous_robots, integer)
-  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
-  integer = 2
-  deleted_robot = famous_robots.delete_at(integer)
+def using_delete_at(array, num)
+  array.delete_at(num)
 end
